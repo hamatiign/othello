eaa1904c3750a78a2b22ed5af44b47b9020a884b
hamatiign <s1f102502687@iniad.org>
2025-04-23 19:50:40 +0900

    色はついたがぐちょぐちょ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, -1, 0, 0, 0],
+    [0, 0, 0, 1, 2, -1, 0, 0],
+    [0, 0, -1, 2, 1, 0, 0, 0],
+    [0, 0, 0, -1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
@@ -26,30 +26,25 @@ export default function Home() {
     [-1, 0],
     [-1, -1],
   ];
+  let i: number = 0;
+  let n = 0;
+  function colorcheck(X: number, Y: number, I: number) {
+    if (board[Y + directions[I][1]] !== undefined) {
+      if (board[Y][X + directions[I][0]] !== undefined) {
+        if (board[Y + directions[I][1]][X + directions[I][0]] === 2 / turnColor) {
+          n++;
+          colorcheck(X + directions[I][0], Y + directions[I][1], I);
+        } else if (board[Y + directions[I][1]][X + directions[I][0]] === turnColor) return;
+        else n = 0;
+      } else n = 0;
+    } else n = 0;
+  }
 
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
 
-    let i: number = 0;
-    let n = 0;
-    function colorcheck(X: number, Y: number, I: number) {
-      if (board[Y + directions[I][1]] !== undefined) {
-        if (board[Y][X + directions[I][0]] !== undefined) {
-          if (board[Y + directions[I][1]][X + directions[I][0]] === 2 / turnColor) {
-            n++;
-            colorcheck(X + directions[I][0], Y + directions[I][1], I);
-          } else if (board[Y + directions[I][1]][X + directions[I][0]] === turnColor) return;
-          else n = 0;
-        } else n = 0;
-      } else n = 0;
-    }
-
     while (i < 8) {
-      //if (
-      //board[y + directions[i][1]][x + directions[i][0]] === 2 / turnColor
-      //)
-      //can_palce = true;
       n = 0;
       colorcheck(x, y, i);
 
@@ -58,8 +53,24 @@ export default function Home() {
 
       i += 1;
     }
-    newBoard[y][x] = turnColor;
+
     setTurnColor(2 / turnColor);
+    setBoard(newBoard);
+
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        i = 0;
+        while (i < 8) {
+          n = 0;
+          colorcheck(row, col, i);
+          if (n > 0 && board[row][col] === 0) newBoard[row][col] = -1;
+
+          if (n === 0 && board[row][col] === -1) newBoard[row][col] = 0;
+          i++;
+        }
+      }
+    }
+    for (let i = 0; i < 8; i++) console.log(board[i]);
     setBoard(newBoard);
   };
 
@@ -69,12 +80,9 @@ export default function Home() {
         {board.map((row, y) =>
           row.map((color, x) => (
             <div key={`${x}-${y}`} onClick={() => clickHandler(x, y)} className={styles.cell}>
-              {color !== 0 && (
-                <div
-                  className={styles.stone}
-                  style={{ background: color === 1 ? `#000` : `#fff` }}
-                />
-              )}
+              {color === 1 && <div className={styles.stone} />}
+              {color === 2 && <div className={styles.stonew} />}
+              {color === -1 && <div className={styles.bluestone} />}
             </div>
           )),
         )}
